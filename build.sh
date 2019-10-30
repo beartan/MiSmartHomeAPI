c1dfceb4d58c8b6af9292c2566886df19941d598
barrierye <barriery@qq.com>
2019-10-30 16:48:25 +0800

    upload modifid PyXiaomiGateway to gitlab && update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+set -v   
+pip install python-miio
+git clone git@gitlab.act.buaa.edu.cn:yebw/pyxiaomigateway.git
 cd PyXiaomiGateway
 pip install wheel
 python setup.py bdist_wheel
 pip install dist/PyXiaomiGateway-0.12.4-py3-none-any.whl
-echo "Successfully installed the modified pyxiaogateway!"
