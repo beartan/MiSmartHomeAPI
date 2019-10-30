bda52b3465e7f7b56f2bb5c030155167a752de52
barrierye <barriery@qq.com>
2019-10-30 16:37:35 +0800

    add PyXiaomiGateway

---
+++ build.sh
@@ -0,0 +1,5 @@
+cd PyXiaomiGateway
+pip install wheel
+python setup.py bdist_wheel
+pip install dist/PyXiaomiGateway-0.12.4-py3-none-any.whl
+echo "Successfully installed the modified pyxiaogateway!"
