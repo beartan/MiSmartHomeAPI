b5c3315438dc851a0cdce16731c9ca5660560502
barrierye <barriery@qq.com>
2019-11-09 14:59:27 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@ pip install django
 pip install sshtunnel
 pip install pymysql
 pip install python-miio
+
+mkdir build
+cd build
 git clone https://github.com/barrierye/PyXiaomiGateway.git
 cd PyXiaomiGateway
 pip install wheel
 python setup.py bdist_wheel
 pip install dist/PyXiaomiGateway-0.12.4-py3-none-any.whl
+cd ./../..
+rm -rf build
